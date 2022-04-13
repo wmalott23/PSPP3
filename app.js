8ebef6ba493b20b6abe2648ddca5c9a137a28627
wmalott23 <101433624+wmalott23@users.noreply.github.com>
2022-04-13 12:42:36 -0700

    Added Problem 1

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 function addArray(){
-    
-}
+    let target = prompt("Enter a number. I will return the indices of two numbers in my array that add up to that number.")
+    let array = [5,17,77,50];
+    let answerArray = array.filter(function(el){
+        for(let i = 0; i<array.length; i++)
+            if(el + array[i] == target) return true;
+        })
+    let resultOne = array.indexOf(answerArray[0]);
+    let resultTwo = array.indexOf(answerArray[1]);
+    let result = `${resultOne} and ${resultTwo}`;
+    return result;
+    }
+
+problemOne = addArray();
+console.log(problemOne)
